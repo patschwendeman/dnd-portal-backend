f572ab2692140e013a4205a3d97b586ec5325ea6
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-08-26 21:34:24 +0200

    Add example data to init.sql

--- src/db/init.sql
+++ src/db/init.sql
@@ -29,3 +29,25 @@ CREATE TABLE battlemap_tracker (
     graphics_id INTEGER REFERENCES graphics(graphics_id),
     graphicsLocked_id INTEGER REFERENCES graphics(graphics_id)
 );
+
+INSERT INTO music (source) VALUES 
+('https://example.com/music1.mp3'), 
+('https://example.com/music2.mp3');
+
+
+INSERT INTO graphics (source) VALUES 
+('https://example.com/graphics1.png'), 
+('https://example.com/graphics2.png');
+
+
+INSERT INTO scene (name, description, screen_type, screen_id, graphics_id) VALUES 
+('Battle at the River', 'A battle scene set near a river.', 'fullscreen', 1, 1);
+
+
+INSERT INTO scene_music (scene_id, music_id) VALUES 
+(1, 1), 
+(1, 2);
+
+
+INSERT INTO battlemap_tracker (locked, graphics_id, graphicsLocked_id) VALUES 
+(false, 1, 2);
