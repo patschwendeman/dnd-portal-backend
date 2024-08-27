cc81553680971af08ad0eeb19e381558fda94aa9
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-08-27 23:30:46 +0200

    Seperate battlemaps from graphics tabels for better structure

--- src/db/init.sql
+++ src/db/init.sql
@@ -15,12 +15,24 @@ CREATE TABLE music (
     source VARCHAR
 );
 
+CREATE TABLE battlemaps (
+    battlemaps_id INTEGER PRIMARY KEY,
+    name VARCHAR,
+    loot VARCHAR,
+    xp VARCHAR,
+    enemies VARCHAR,
+    locked BOOLEAN NOT NULL,
+    source_clear VARCHAR,
+    source_locked VARCHAR
+);
+
 CREATE TABLE scene (
     scene_id INTEGER PRIMARY KEY,
     name VARCHAR,
-    description TEXT,
+    description VARCHAR,
     graphics_wall_id INTEGER REFERENCES graphics_wall(graphics_wall_id),
-    graphics_ground_id INTEGER REFERENCES graphics_ground(graphics_ground_id)
+    graphics_ground_id INTEGER REFERENCES graphics_ground(graphics_ground_id),
+    battlemaps_id INTEGER REFERENCES battlemaps(battlemaps_id)
 );
 
 CREATE TABLE scene_music (
@@ -29,31 +41,28 @@ CREATE TABLE scene_music (
     PRIMARY KEY (scene_id, music_id)
 );
 
-CREATE TABLE battlemap_tracker (
-    battlemapTracker_id INTEGER PRIMARY KEY,
-    loot VARCHAR,
-    xp VARCHAR,
-    enemies VARCHAR,
-    locked BOOLEAN NOT NULL,
-    graphics_id INTEGER REFERENCES graphics_ground(graphics_ground_id),
-    graphicsLocked_id INTEGER REFERENCES graphics_ground(graphics_ground_id)
-);
+
 
 INSERT INTO graphics_ground (graphics_ground_id, name, source) VALUES 
-(1, 'Chamber 1 battlefiel', 'https://example.com/ground.png'), 
-(2, 'Chamber 1 battlefield Locked', 'https://example.com/ground_locked.png');
+(1, 'Forest', 'https://example.com/Ground_Forest.png'), 
+(2, 'Shop', 'https://example.com/Ground_Shop.png');
 
 INSERT INTO graphics_wall (graphics_wall_id, name, source) VALUES 
-(1, 'Chamber 1 wall', 'https://example.com/wall.png');
+(1, 'Forest', 'https://example.com/Wall_Forest.png'), 
+(2, 'Shop', 'https://example.com/Wall_Shop.png'),
+(3, 'Champer', 'https://example.com/Champer.png');
 
 INSERT INTO music (music_id, source) VALUES 
 (1, 'https://example.com/music1.mp3');
 
-INSERT INTO scene (scene_id, name, description, graphics_ground_id, graphics_wall_id) VALUES 
-(1, 'Battle at the River', 'A battle scene set near a river.', 1, 1);
+INSERT INTO battlemaps (battlemaps_id, name, loot, xp, enemies, locked, source_clear, source_locked) VALUES 
+(1, 'First Room', 'Old Fish', '200 xp', 'Archer', false, 'https://example.com/battle.png', 'https://example.com/battle_locked.png');
+
+
+INSERT INTO scene (scene_id, name, description, graphics_ground_id, graphics_wall_id, battlemaps_id) VALUES 
+(1, 'Forest', 'A lonely Forest.', 1, 1, NULL),
+(2, 'Forest', 'A lonely Forest.', 2, 2, NULL),
+(3, 'Battle', 'First Room.', NULL, 3, 1);
 
 INSERT INTO scene_music (scene_id, music_id) VALUES 
 (1, 1);
-
-INSERT INTO battlemap_tracker (battlemapTracker_id, loot, xp, enemies, locked, graphics_id, graphicsLocked_id) VALUES 
-(1, 'Old Fish', '200 xp', 'Archer', false, 1, 2);
