5832c42f206a10d406816114da58e9cb1f23cf4e
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2024-08-31 00:15:23 +0200

    Add API routes

---
+++ src/routes/battlemaps.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy.orm import Session
+from services.battlemaps import BattlemapService
+from db.database import get_db
+
+
+battlemaps_router = APIRouter()
+
+
+@battlemaps_router.get("/battlemaps")
+def read_battlemaps(filter: Optional[bool] = Query(None), db: Session = Depends(get_db)):
+    battlemap = BattlemapService.read_battlemaps(db, filter)
+    if battlemap is None:
+        raise HTTPException(status_code=404, detail="Battlemap not found")
+    return battlemap
+
+@battlemaps_router.get("/battlemaps/{battlemap_id}")
+def read_battlemaps_by_id(battlemap_id, db: Session = Depends(get_db)):
+    battlemap = BattlemapService.read_battlemaps_by_id(db, battlemap_id)
+    if battlemap is None:
+        raise HTTPException(status_code=404, detail="Battlemap not found")
+    return battlemap
---
+++ src/routes/scenes.py
@@ -0,0 +1,23 @@
+from typing import Optional
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from services.scenes import SceneService
+from db.database import get_db
+
+
+scenes_router = APIRouter()
+
+
+@scenes_router.get("/scenes")
+def read_scene(db: Session = Depends(get_db)):
+    scene = SceneService.read_scenes(db)
+    if scene is None:
+        raise HTTPException(status_code=404, detail="Scene not found")
+    return scene
+
+@scenes_router.get("/scenes/{scene_id}")
+def read_scene(scene_id, db: Session = Depends(get_db)):
+    scene = SceneService.read_scene_by_id(db, scene_id)
+    if scene is None:
+        raise HTTPException(status_code=404, detail="Scene not found")
+    return scene
