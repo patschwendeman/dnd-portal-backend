c092c1c3cde3c31c97db352a158e7d24cec19e5c
Patrick Schwendemann <patrick.schwendemann@bamero.de>
2025-04-24 11:42:27 +0200

    Combine sidemaps and mainmaps service layer into one generic mainmaps service layer

--- src/services/maps.py
+++ src/services/maps.py
@@ -1,35 +1,27 @@
-import json
-from typing import Optional
 from sqlalchemy.orm import Session
-from src.db.models import BattleMap
-from src.services.battlemaps import BattlemapService
-from src.services.sidemaps import SidemapsService
+from src.db.crud import read_all
+from src.db.models import Scene
 
+# pylint: disable=too-few-public-methods
 
 class MapsService:
-
     @staticmethod
-    def read_maps(filter_condition: Optional[bool], db: Session, maptype) -> json:
-        if maptype == "battlemap":
-            maps = BattlemapService.read_battlemaps(db, filter_condition)
-        elif maptype == "sidemap":
-            maps = SidemapsService.read_sidemaps(db)
-        else:
-            raise ValueError("Maps not found")
-        if not maps:
-            raise ValueError("Maps not found")
-        return maps
+    def read_maps(db: Session, maptype: str):
+        if maptype not in ("main", "side"):
+            raise ValueError("Maptype must be either 'main' or 'side'")
 
-    @staticmethod
-    def read_map_by_id(db: Session, map_id: int) -> json:
-        map_element = BattlemapService.read_battlemap_by_id(db, map_id)
-        if not map_element:
-            raise ValueError("Map not found")
-        return map_element
+        filter_main = maptype == "main"
 
-    @staticmethod
-    def update_map(db: Session, map_id: int, data) -> BattleMap:
-        updated_map = BattlemapService.update_battlemap(db, map_id, data)
-        if not updated_map:
-            raise ValueError("updated map not found")
-        return updated_map
+        scenes = read_all(db, Scene)
+        if not scenes:
+            raise ValueError("Scenes not found")
+
+        filtered_maps = []
+        for scene in scenes:
+            if scene.main is filter_main:
+                filtered_maps.append({
+                    'id': scene.id,
+                    'source': scene.graphics_wall.source
+                })
+
+        return filtered_maps
